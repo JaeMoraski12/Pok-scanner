1c16c635a7f4abfbd0c633e2122d20b4905b9dbd
Jaelind Moraski <jemoraski@wm.edu>
2026-04-06 14:15:02 -0400

    Add the AiImageAllocation for the images used in the AI learning

--- PokemonDatabase.sql
+++ PokemonDatabase.sql
@@ -68,4 +68,15 @@ CREATE TABLE GameAllocation (
     PRIMARY KEY (pokemon_id, game_id)
 )
 
+CREATE TABLE AiImagesChart (
+    ai_image_id INT PRIMARY KEY AUTO_INCREMENT,
+    image_name VARCHAR(500) NOT NULL
+)
+
+CREATE TABLE AiAllocation (
+    pokemon_id INTEGER REFERENCES Pokemon(pokemon_id),
+    ai_image_id INTEGER REFERENCES AiImagesChart(ai_image_id)
+    PRIMARY KEY (pokemon_id, ai_image_id)
+)
+
 
